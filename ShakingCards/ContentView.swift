f2bb8ef1b71d8a8a1d7d44b236ecd8bb2351fffb
Anna Panova <annapanovadev@gmail.com>
2025-04-11 10:52:59 +0200

    Initial Commit

---
+++ ShakingCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShakingCards
+//
+//  Created by Anna Panova on 11.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShakingCards/ShakingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakingCardsApp.swift
+//  ShakingCards
+//
+//  Created by Anna Panova on 11.04.25.
+//
+
+import SwiftUI
+
+@main
+struct ShakingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
